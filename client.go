d1339d017bfed911b6426cfdd0ceffba3fd4341c
Jahvon Dockery <jahvondockery@gmail.com>
2024-07-16 21:52:26 -0400

    (refactor) split schema

--- client.go
+++ client.go
@@ -1,3 +1,9 @@
 package flair
 
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=config.yaml api.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=auth/config.yaml auth/api.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=bridges/config.yaml bridges/api.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=pucks/config.yaml pucks/api.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=rooms/config.yaml rooms/api.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=structures/config.yaml structures/api.yaml
+
+const APIURL = "https://api.flair.co"
