b107685368c7b07f37b50fe91f1f74077cd2c4b1
Jahvon Dockery <jahvondockery@gmail.com>
2024-08-31 16:52:53 -0400

    (refactor) add oauth helpers in place of client

--- client.go
+++ client.go
@@ -1,5 +1,7 @@
 package flair
 
+import "golang.org/x/oauth2"
+
 //go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=auth/config.yaml auth/api.yaml
 //go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=bridges/config.yaml bridges/api.yaml
 //go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=pucks/config.yaml pucks/api.yaml
@@ -7,3 +9,35 @@ package flair
 //go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=structures/config.yaml structures/api.yaml
 
 const APIURL = "https://api.flair.co"
+
+var Endpoint = oauth2.Endpoint{
+	AuthURL:  APIURL + "/oauth/authorize",
+	TokenURL: APIURL + "/oauth/token",
+}
+
+const (
+	// User Scope - view user names and settings
+
+	UserViewScope  = "users.view"
+	UsersEditScope = "users.edit"
+
+	// Structures Scope - view the user's Flair home setup and configuration (includes rooms and hvac-units)
+
+	StructuresViewScope = "structures.view"
+	StructuresEditScope = "structures.edit"
+
+	// Pucks Scope - view Pucks associated with this user
+
+	PucksViewScope = "pucks.view"
+	PucksEditScope = "pucks.edit"
+
+	// Vents Scope - view Vents associated with this user
+
+	VentsViewScope = "vents.view"
+	VentsEditScope = "vents.edit"
+
+	// Thermostats Scope - view smart thermostats associated with this user
+
+	ThermostatsViewScope = "thermostats.view"
+	ThermostatsEditScope = "thermostats.edit"
+)
